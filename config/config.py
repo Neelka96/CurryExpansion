0417d3372859c539f69da13bb02e1bf62d1ac875
Neel Agarwal <neelkagarwal@gmail.com>
2025-05-03 01:58:04 -0500

    Refactor Settings class to improve field definitions and validation; add computed properties for dynamic storage and log file paths

--- config/config.py
+++ config/config.py
@@ -1,81 +1,96 @@
-from typing import Literal, Any
+from typing import Literal, Any, TypeAlias
 from pathlib import Path
-from datetime import timedelta
 
-from pydantic import field_validator, model_validator
+from pydantic import SecretStr, model_validator, field_validator, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-# API keys removed from settings as they're included in expansion of the YAML
+# Type aliases to make improve readability of Settings fields
+Envs: TypeAlias = Literal['development', 'production']
+LogLevels: TypeAlias    = Literal['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL', 'NOTSET']
+
+# Quick FYI for reading of configuration
+# Pydantic's BaseSettings validators works in the flow of: 
+# (mode = 'before') model -> field -> (mode = 'after') field -> model
+
 class Settings(BaseSettings):
-    APP_ENV: str = 'development'
-    LOG_LEVEL:          Literal['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL', 'NOTSET'] | None = 'INFO' # Default by env -> (Prod) INFO | (Dev) DEBUG
-    DEBUG:              bool        = True
-    SLEEP:              int         = 1
-    UPDATE_INTERVAL:    int | None  = None
+    # Base options
+    model_config = SettingsConfigDict(env_file = '.env', case_sensitive = False)
 
-    STORAGE:            Path | None = None  # Default by env    -> (Prod) /mnt/shared   | (Dev) /~/resources
-    LOG_FILE:           Path | None = None  
-    AZURE_CONN:         str | None  = None  # Default of None   -> (Prod) Required str  | (Dev) None
-    STORAGE_ACCT_CONN:  str | None  = None
+    # API keys removed from settings as they're included in expansion of the YAML
+    # Basic App/Env Configurations
+    app_env:        Envs                = 'development'
+    debug:          bool                = True
 
-    ENGINE_NAME:        str | None  = 'curryexpanse'
-    ENGINE_PATH:        Path | None = None
-    ENGINE_URI:         Path | None = None
+    # Log Configurations
+    log_level:      LogLevels           = 'INFO'
+    log_name:       str                 = 'app.log'
 
+    # Drives available to store persistent data and the drive chosen by user or default for environment
+    mnt_drive:      Path                = Path('/mnt/shared')
+    loc_drive:      Path                = Path(__file__).resolve().parent / 'resources'
 
-    model_config = SettingsConfigDict(
-        env_file = '.env'
-        # ,env_ignore_empty = True
-        ,case_sensitive = False
-    )
+    # CaaS secrets
+    caas_conn:      SecretStr | None    = None  # Default of None -> (Prod) **REQUIRED | (Dev) None
+    caas_mnt_conn:  SecretStr | None    = None  # ^^^ Same as above
 
-    @field_validator('LOG_LEVEL', mode = 'before')
-    def uppercase_log_level(cls, v: Any) -> Any:
-        if isinstance(v, str):
-            return v.upper().strip()
-        return v
+    # PostgreSQL URI construction parts - db_user_pass ALWAYS REQUIRED
+    db_user_name:   str                 = 'curryexp'
+    db_user_pass:   SecretStr
+    db_name:        str                 = 'curryexpander'
+    db_host:        str                 = 'localhost'
+    db_port:        int                 = 5432
 
-    @field_validator('APP_ENV', mode = 'before')
-    def lowercase_app_env(cls, v: Any) -> Any:
-        if isinstance(v, str):
-            return v.lower().strip()
-        return v
-
-    @field_validator(mode = 'after')
-    def new_post_type_coersion_here(cls, v: Any) -> Any:
-        ...
+    # -- Pre parsing field validators --
+    # Specifically lowercases on app env
+    @classmethod
+    @field_validator('app_env', mode = 'before')
+    def lowercase_strings(cls, v: Any) -> Any:
+        return v.lower() if isinstance(v, str) else v
     
+    # Specifically uppercases on log level
+    @classmethod
+    @field_validator('log_level', mode = 'before')
+    def uppercase_strings(cls, v: Any) -> Any:
+        return v.upper() if isinstance(v, str) else v
+    
+    # Strips whitespace on string values that may be alphanumeric
+    @classmethod
+    @field_validator('*', mode = 'before')
+    def strip_all_strings(cls, v: Any) -> Any:
+        return v.strip() if isinstance(v, str) else v
+    
+    # -- Post parsing model validation --
     @model_validator(mode = 'after')
-    def dynamic_settings(cls, v: dict) -> dict:
-        env = v['APP_ENV']  # Shorthand for environment setting
+    def extra_post_validation(self) -> 'Settings':
+        # Checks for presence of cloud service connection strings and raises if error if any are missing
+        if self.app_env == 'production' and (self.caas_conn is None or self.caas_mnt_conn is None):
+            raise TypeError('CaaS connection strings required in production settings.')
+        return self
 
-        # Raises error if the Azure Conn Str is mising in production
-        if env == 'production' and v['AZURE_CONN'] is None:
-            raise TypeError('Cloud provider connection string required for production environments.')
+    # -- Post parsing property fields dumped into Pydantic --
+    # storage property comes from the active environment and whether or not the drive exists; if production mount doesn't exist switch to development mount
+    @computed_field
+    @property
+    def storage(self) -> Path:
+        if self.app_env == 'production' and self.mnt_drive.exists() and self.mnt_drive.is_dir():
+            return self.mnt_drive
+        else:
+            if not self.loc_drive.exists():
+                self.loc_drive.mkdir(parents = True, exist_ok = True)
+            return self.loc_drive
 
-        # # Dynamic default for log level by env
-        # if v['LOG_LEVEL'] is None:
-        #     v['LOG_LEVEL'] = 'INFO' if env == 'production' else 'DEBUG'
-        
-        # Dynamic default for storage directory by env
-        if v['STORAGE'] is None:
-            v['STORAGE'] = Path('/mnt/shared') if env == 'production' else Path(__file__).resolve().parent / 'resources'
+    # log_file property comes from other dynamic property configured during runtime
+    @computed_field
+    @property
+    def log_file(self) -> Path:
+        return self.storage / self.log_name
 
-        # 
-        if v['LOG_FILE'] is None: 
-            v['LOG_FILE'] = 'app.log'
-        v['LOG_FILE'] = v['STORAGE'] / v['LOG_FILE']
-
-        if v['ENGINE_PATH'] is None:
-            v['ENGINE_PATH'] = v['STORAGE'] / 'postgresql+psycopg2://local:host@localhost:5432/curryexpanse'
-
-#     def _load_engine(self):
-#         self.ENGINE_PATH = self.STORAGE / self.db_name
-#         self.ENGINE_URI = f'sqlite:///{self.ENGINE_PATH}'
-#         self.UPDATE_INTERVAL = timedelta(weeks = 2)
-#         self.FASTFOOD_CSV = self.STORAGE / 'fastfood.csv'
-#         self.POPULATION_CSV = self.STORAGE / 'census_population.csv'
-#         return self
+    # engine_uri property comes from other static environment variable configurations available
+    @computed_field
+    @property
+    def engine_uri(self) -> str:
+        eng = 'postgresql+psycopg2'
+        return f'{eng}://{self.db_user_name}:{self.db_user_pass.get_secret_value()}@{self.db_host}:{self.db_port}/{self.db_name}'
 
 
 # EOF
